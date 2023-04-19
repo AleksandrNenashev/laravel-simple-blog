bed1ed786ac20830deaf9424799016ba5ba1d3c5
AleksandrNenashev <nenashev88@gmail.com>
2023-04-20 03:28:26 +0400

    change .htaccess .env, move .htaccess

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,5 +4,5 @@
 
 @section('content')
     <h1>О нас</h1>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam explicabo, fuga debitis labore voluptatem tempore aliquid possimus expedita odit commodi.</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor, sequi. Ducimus, cum autem cumque eum impedit iusto id, optio maxime debitis nam tenetur laboriosam officia veritatis neque excepturi quibusdam ab adipisci delectus et hic sed necessitatibus. Dignissimos non ut ipsum deleniti quae ipsam laborum temporibus minus quod! Est voluptas eligendi quaerat molestiae voluptates. Amet, iusto. Voluptates doloremque reiciendis, ducimus repellendus deserunt quaerat dolore minima temporibus qui, iure dignissimos quod nisi debitis similique alias laboriosam! Corporis omnis sed sequi expedita ratione, eos itaque nesciunt ducimus quam neque excepturi similique voluptatum aliquid, assumenda labore, voluptates consectetur commodi accusamus voluptatibus doloribus aperiam adipisci.</p>
 @endsection
